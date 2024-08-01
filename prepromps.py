cad5fdaf6b96e0092ca3f6b367aacd4044daee5a
Azerty-Labs <75258525+azerty-labs@users.noreply.github.com>
2024-08-01 23:31:43 +0200

    add lisa youtube summary

--- prepromps.py
+++ prepromps.py
@@ -13,5 +13,9 @@ preprompts = {
         "You are Carla, a UI/UX designer with expertise in HTML, React, and CSS. You provide insights and solutions to improve web interfaces and user experiences through effective design and coding practices.",
         "Hello, I'm Carla, your UI/UX design specialist. Let’s create stunning, user-friendly interfaces and experiences for your web projects!",
         "./assets/banner_carla.png"
+    ],
+    "Lisa - Youtube Summary": [
+        "You're Lisa a specialist in text summaries of YouTube videos. On your next release, you'll produce a markdown summary with the following sections: (Introduction, Key points, Supporting details, Conclusion, Additional information (References or additional sources or resources). here's the audio transcript. NEVER MENTION THAT YOU'RE RELYING SOLELY ON AUDIO", "",
+        "./assets/banner_lisa.png"
     ]
 }
--- streamlit-app.py
+++ streamlit-app.py
@@ -1,9 +1,11 @@
-from prepromps import preprompts
-import streamlit as st
 import os
 import json
+import yt_dlp
+import streamlit as st
+import whisper
 from hugchat import hugchat
 from hugchat.login import Login
+from prepromps import preprompts
 
 # Define the cookie path directory
 COOKIE_PATH_DIR = "./cookies/"
@@ -12,6 +14,8 @@ COOKIE_FILE_PATH = os.path.join(COOKIE_PATH_DIR, "cookies.json")
 # Ensure the cookie directory exists
 if not os.path.exists(COOKIE_PATH_DIR):
     os.makedirs(COOKIE_PATH_DIR)
+
+model = whisper.load_model("base")
 
 
 def authenticate(email: str, password: str):
@@ -42,8 +46,17 @@ def save_cookies(cookies):
         json.dump(cookies, file)
 
 
-def main():
+def youtube_downloader(link, dir_path):
+    """
+    Download audio from a YouTube link.
+    """
+    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': os.path.join(dir_path, '%(id)s.wav')}) as audio:
+        info_dict = audio.extract_info(link, download=True)
+        id = info_dict['id']
+        return os.path.join(dir_path, str(id) + ".wav")
 
+
+def main():
     if 'chatbot' not in st.session_state:
         st.title("Personal assistant")
         st.subheader("Accessing personal assistant using HuggingChat")
@@ -81,39 +94,63 @@ def main():
 
     # Pre-prompt setting
     st.sidebar.subheader('Assistants')
-
     st.sidebar.markdown(
         'Select an assistant to get expert help tailored to your needs. Each assistant is here to provide specialized support and guidance.')
 
     # Assistant type selection
-    assistant_type = st.sidebar.selectbox(
+    st.session_state.assistant_type = st.sidebar.selectbox(
         "Select an Assistant:",
         preprompts.keys()
     )
 
-    allow_web_search = st.sidebar.checkbox(
+    st.session_state.allow_web_search = st.sidebar.checkbox(
         "Allow web search")
 
+    # Audio download option
+    if st.session_state.assistant_type == "Youtube Summary":
+        youtube_link = st.sidebar.text_input("YouTube Link for Audio")
+        if st.sidebar.button("Process Link", use_container_width=True):
+            if youtube_link:
+                with st.spinner("processing video..."):
+                    audio_file = youtube_downloader(
+                        youtube_link, COOKIE_PATH_DIR)
+                    result = model.transcribe(audio_file)
+                    result = result['text']
+                    st.session_state.TTS_result = result
+                    st.sidebar.success(f"Video processed")
+            else:
+                st.sidebar.error("Please provide a valid YouTube link.")
+    else:
+        st.session_state.TTS_result = ""
+
     ini_assistant = st.sidebar.button(
-        "Initialise assistant", type="primary", use_container_width=True)
+        "Initialize Assistant", type="primary", use_container_width=True)
 
     # Create ChatBot instance
     if ini_assistant:
-        with st.spinner("Initializing Chatbot..."):
-            if 'chatbot' not in st.session_state:
-                st.session_state.chatbot = hugchat.ChatBot(cookies=cookies)
-                print(st.session_state.chatbot.chat(
-                    preprompts[assistant_type][0]+" If it is ok for you respond 'OK!'", web_search=allow_web_search))
+        if st.session_state.assistant_type != "Youtube Summary":
+            with st.spinner("Initializing Chatbot..."):
+                if 'chatbot' not in st.session_state:
+                    st.session_state.chatbot = hugchat.ChatBot(cookies=cookies)
+                    print(st.session_state.chatbot.chat(
+                        preprompts[st.session_state.assistant_type][0] + " If it is ok for you respond 'OK!'", web_search=st.session_state.allow_web_search))
+        if st.session_state.assistant_type == "Youtube Summary":
+            with st.spinner("Initializing Chatbot..."):
+                if 'chatbot' not in st.session_state:
+                    st.session_state.chatbot = hugchat.ChatBot(cookies=cookies)
+                    response = st.session_state.chatbot.chat(
+                        preprompts[st.session_state.assistant_type][0] + "\n" + st.session_state.TTS_result + "\n Now, return the summaries please.", web_search=st.session_state.allow_web_search)
+                    preprompts[st.session_state.assistant_type][1] = response
 
     if 'chatbot' in st.session_state:
-        st.image(preprompts[assistant_type][2])
+        st.image(preprompts[st.session_state.assistant_type][2])
 
         # Chat interface
 
         # Store LLM generated responses
         if "messages" not in st.session_state.keys():
             st.session_state.messages = [
-                {"role": "assistant", "content": preprompts[assistant_type][1]}]
+                {"role": "assistant", "content": preprompts[st.session_state.assistant_type][1]}]
 
         # Display chat messages
         for message in st.session_state.messages:
@@ -133,7 +170,7 @@ def main():
                 try:
                     with st.spinner("Thinking..."):
                         response = st.session_state.chatbot.chat(
-                            prompt, web_search=allow_web_search)
+                            prompt, web_search=st.session_state.allow_web_search)
                         st.markdown(response)
                 except:
                     st.markdown(
